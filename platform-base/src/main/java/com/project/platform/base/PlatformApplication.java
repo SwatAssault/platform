a8da1f7d2d2407565e87f17e5e1879cabbf26816
Ruslan <43928175+SwatAssault@users.noreply.github.com>
2023-06-18 04:45:01 +0500

    PLTF-3: Initial project startup - создан пустой проект на Spring Boot

---
+++ platform-base/src/main/java/com/project/platform/base/PlatformApplication.java
@@ -0,0 +1,37 @@
+package com.project.platform.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class PlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformApplication.class, args);
+    }
+
+    // TODO не работает. попытка открывать браузер после старта сервера
+//    @EventListener({ApplicationReadyEvent.class})
+//    public void applicationReadyEvent() {
+//        System.out.println("Application started ... launching browser now");
+//        browse("http://localhost:{port}/");
+//    }
+//
+//    public static void browse(String url) {
+//        if (Desktop.isDesktopSupported()) {
+//            Desktop desktop = Desktop.getDesktop();
+//            try {
+//                desktop.browse(new URI(url));
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        } else {
+//            Runtime runtime = Runtime.getRuntime();
+//            try {
+//                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+}
