cd0154947533b4e0412707f634af81e2340849ec
Ruslan <43928175+SwatAssault@users.noreply.github.com>
2023-06-19 21:58:36 +0500

    PLTF-6: Интеграция с PoetryDB. Часть 1 - добавлен jackson в проект - использовал dto при получении авторов - добавил логгирование

---
+++ platform-base/src/main/java/com/project/platfrom/base/exception/JsonDeserializeException.java
@@ -0,0 +1,9 @@
+package com.project.platfrom.base.exception;
+
+
+public class JsonDeserializeException extends RuntimeException {
+
+    public JsonDeserializeException(String message) {
+        super(message);
+    }
+}
--- platform-base/src/main/java/com/project/platfrom/base/http/HttpRequester.java
+++ platform-base/src/main/java/com/project/platfrom/base/http/HttpRequester.java
@@ -1,5 +1,7 @@
 package com.project.platfrom.base.http;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class HttpRequester {
+    private final Logger logger = LoggerFactory.getLogger(HttpRequester.class);
 
     private final RestTemplate restTemplate;
 
@@ -20,6 +23,7 @@ public class HttpRequester {
 
     public ResponseEntity<String> get(String url, Object object) {
         HttpEntity<Object> request = object != null ? new HttpEntity<>(object) : null;
+        logger.info("Requesting external URL {} with request payload {}", url, object);
         return restTemplate.exchange(
                 url,
                 HttpMethod.GET,
